a9e9c19e734f06a7246dbe206debfbdf6db115ec
Mihai Vasilescu <44552815+Mihai-V@users.noreply.github.com>
2020-05-24 22:39:59 +0300

    Add files via upload

---
+++ laborator 10 - tema 1/cmake-build-debug/CMakeFiles/laborator_10___tema_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laborator_10___tema_1.dir/main.cpp.obj"
+  "laborator_10___tema_1.pdb"
+  "laborator_10___tema_1.exe"
+  "laborator_10___tema_1.exe.manifest"
+  "laborator_10___tema_1.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laborator_10___tema_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
